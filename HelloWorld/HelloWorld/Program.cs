270cd2075a934a283c94103d8f3f7d51165cd382
manzar <manzar79@gmail.com>
2014-06-26 11:01:52 -0400

    code entered
    code entered

---
+++ HelloWorld/HelloWorld/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HelloWorld
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string inputReadLine;
+
+            Console.WriteLine("Hello World!");
+            inputReadLine = Console.ReadLine();
+            Console.WriteLine(inputReadLine);
+            Console.ReadLine();
+        }
+    }
+}
